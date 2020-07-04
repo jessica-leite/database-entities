bd1df6525074b30927a72d7ad9f4f23f003152c9
Jessica Leite <jessica.mleite@gmail.com>
2020-07-04 19:42:54 +0100

    Context updated

--- Source/Models/CodenationContext.cs
+++ Source/Models/CodenationContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Source.Models;
 using Source.Models.Mapping;
 
 namespace Codenation.Challenge.Models
@@ -6,6 +7,11 @@ namespace Codenation.Challenge.Models
     public class CodenationContext : DbContext
     {
         public DbSet<Company> Companies { get; set; }
+        public DbSet<Source.Models.Challenge> Challenges { get; set; }
+        public DbSet<Acceleration> Accelerations { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Submission> Submissions { get; set; }
+        public DbSet<Candidate> Candidates { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
